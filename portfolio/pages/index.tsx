2783d1381368a9a561a2d7cd3196599450256083
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-01-22 04:35:56 +0530

    Navigation Created

---
+++ portfolio/pages/index.tsx
@@ -0,0 +1,12 @@
+import Nav from '@/components/Nav';
+import React from 'react'
+
+const HomePage = () => {
+  return <div className='overlflow-x-hidden'>
+    <div>
+      <Nav />
+    </div>
+  </div>
+}
+
+export default HomePage;
