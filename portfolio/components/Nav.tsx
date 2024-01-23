41d25cc17da83d91f690dd063c9b56ea1b9752a2
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-01-24 01:02:06 +0530

    Update Nav.tsx

--- portfolio/components/Nav.tsx
+++ portfolio/components/Nav.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Nav = () => {
 	return (
 		<div className='w-[100%] top-0 h-[10vh] bg-[#141C27] shadow-md'>
-			<div className="flex items-center justify-between w-[80%] mx-auto h-[100%]">
-				<h1 className="flex-[0.6] cursor-pointer text-[25px] text-white font-bold">Portfolio.</h1>
+			<div className="flex items-center justify-between w-[92%] mx-auto h-[100%]">
+				<h1 className="flex-[0.8] cursor-pointer text-[25px] text-white font-bold">Portfolio.</h1>
 				<div className="nav-link">Home</div>
 				<div className="nav-link">About</div>
 				<div className="nav-link">Services</div>
