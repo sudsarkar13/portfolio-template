f282fda966caefed9686bd6794200d4c9394ed58
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-01-25 00:35:50 +0530

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="public/favicon.ico" />
+        {/* public/favicon.ico */}
+        {/* Other head elements can also be included here */}
+      </Head>
       <body>
         <Main />
         <NextScript />
