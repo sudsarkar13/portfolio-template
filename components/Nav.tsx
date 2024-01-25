7ae84d749c2013221a3c4834b4f4c2e01f58b8a7
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-01-26 04:38:45 +0530

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,12 +7,12 @@ const Nav = () => {
 				<h1 className='flex-[0.8] cursor-pointer text-[25px] text-white font-bold'>
 					Port<span className="text-yellow-400">folio.</span>
 				</h1>
-				<div className='nav-link'>Home</div>
-				<div className='nav-link'>About</div>
-				<div className='nav-link'>Services</div>
-				<div className='nav-link'>Projects</div>
-				<div className='nav-link'>Blog</div>
-				<div className='nav-link'>Contact</div>
+<div className='nav-link uppercase'>Home</div>
+				<div className='nav-link uppercase'>About</div>
+				<div className='nav-link uppercase'>Services</div>
+				<div className='nav-link uppercase'>Projects</div>
+				<div className='nav-link uppercase'>Blog</div>
+				<div className='nav-link uppercase'>Contact</div>
 			</div>
 		</div>
 	);
