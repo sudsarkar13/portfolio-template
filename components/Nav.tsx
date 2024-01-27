1d9d9d08f8f3516097782043a16910698d0c8a54
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-01-27 20:43:39 +0530

    Updated Mobile Navigation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+import { Bars3Icon } from "@heroicons/react/20/solid";
 
-const Nav = () => {
+interface Props {
+	openNav: () => void;
+}
+
+const Nav = ({openNav}:Props) => {
 	return (
 		<div className='w-[100%] top-0 h-[9vh] bg-[#141C27] shadow-md'>
 			<div className='flex items-center justify-between w-[92%] mx-auto h-[100%]'>
 				<h1 className='flex-[0.8] cursor-pointer text-[25px] text-white font-bold'>
-					Port<span className="text-yellow-400">folio.</span>
+					Port<span className='text-yellow-400'>folio.</span>
 				</h1>
-<div className='nav-link uppercase'>Home</div>
+				<div className='nav-link uppercase'>Home</div>
 				<div className='nav-link uppercase'>About</div>
 				<div className='nav-link uppercase'>Services</div>
 				<div className='nav-link uppercase'>Projects</div>
 				<div className='nav-link uppercase'>Blog</div>
 				<div className='nav-link uppercase'>Contact</div>
+				<div onClick={openNav}>
+					<Bars3Icon className='w-[2rem] md:hidden h-[2rem]w cursor-pointer text-white font-extrabold' />
+				</div>
 			</div>
 		</div>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "@/components/Nav";
+import MobileNav from "../components/MobileNav";
 
 const HomePage = () => {
+	const [nav, setNav] = useState(false);
+	const openNav = () => setNav(true);
+	const closeNav = () => setNav(false);
+
 	return (
 		<div className='overflow-x-hidden'>
 			<div>
-				<Nav />
+				<MobileNav nav={nav} closeNav={closeNav} />
+				<Nav openNav={openNav} />
 			</div>
 		</div>
 	);
