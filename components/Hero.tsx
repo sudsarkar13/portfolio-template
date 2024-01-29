30355cf41d4f3f25d98b0ea4a1d09731be892eef
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-01-29 10:04:07 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Particle from "./Particle";
 // import Error404 from "./errors/Error404";
@@ -5,8 +6,13 @@ import Particle from "./Particle";
 
 const Hero = () => {
 	return (
-		<div className="h-[91vh] bg-[url('/images/banner.jpg')] bg-cover bg-center ">
+		<div className="h-[90vh] bg-[url('/images/banner.jpg')] bg-cover bg-center ">
 			<Particle />
+			<div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
+				<h1 className="text-[35px] md:text-[50px] text-white font-extrabold">
+					HI, I'M <span className="text-yellow-400">SUDEEPTA!</span>
+				</h1>
+			</div>
 		</div>
 	);
 };
