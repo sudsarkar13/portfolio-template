7db5d3910373177dc9650fc557a056043d05a8f3
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-01-29 18:13:32 +0530

    Update MobileNav.tsx

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -11,7 +11,8 @@ const MobileNav = ({ nav, closeNav }: Props) => {
 
 	return (
 		<div
-			className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[10000] bg-[#09101a]`}
+			// className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[10000] bg-[#09101a]`}
+			className={`fixed ${navAnimation} transform transition-all duration-300 top-[5rem] left-0 right-0 bottom-0 z-[10000] bg-[#09101a]`}
 		>
 			<div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
 				<div className='nav-link-mobile uppercase'>Home</div>
