8aa447ca5d7673ffff2727105c3096034090d6cf
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-01 21:27:35 +0530

    Header Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { useState } from "react";
 import Nav from "@/components/Nav";
 import MobileNav from "@/components/MobileNav";
 import Hero from "@/components/Hero";
+import Head from "next/head";
 
 const HomePage = () => {
 	const [nav, setNav] = useState(false);
@@ -10,6 +12,14 @@ const HomePage = () => {
 
 	return (
 		<div className='overflow-x-hidden'>
+			<Head>
+				<title>Sudeepta's Portfolio</title>
+				<link
+					rel='icon'
+					href='/public/favicon/favicon.ico'
+					type='image/x-icon'
+				/>
+			</Head>
 			<div>
 				{/* Navbar */}
 				<MobileNav nav={nav} closeNav={closeNav} />
