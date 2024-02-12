a42f453900f70850a9faf88ce743518ec9c656bd
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-12 11:18:24 +0530

    Update TextEffect.tsx

--- components/TextEffect.tsx
+++ components/TextEffect.tsx
@@ -2,7 +2,24 @@ import React from "react";
 import { TypeAnimation } from "react-type-animation";
 
 const TextEffect = () => {
-	return <div>TextEffect</div>;
+	return (
+		<TypeAnimation
+			sequence={[
+				// Same substring at the start will only be typed out once, initially
+				"Coder",
+				1500, // wait 1s before replacing "Mice" with "Hamsters"
+				"Web Developer",
+				1500,
+				"Designer",
+				1500,
+				"Content Writer, Blogger & Editor",
+				1500,
+			]}
+			speed={50}
+			className='text-[2rem] md:text-[3rem] text-[#55e6a5] font-bold uppercase'
+			repeat={Infinity}
+		/>
+	);
 };
 
 export default TextEffect;
