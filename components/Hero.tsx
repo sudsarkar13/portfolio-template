30d525de5df86ae10dc5f6f099319a96d89663c0
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-12 13:48:56 +0530

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,7 +18,7 @@ const Hero = () => {
 					</h1>
 					<TextEffect />
 					<p className='mt-[2rem] text-[20px] text-[#ffffff92]'>
-						A highly motivated B.Tech graduate with a passion for Web
+						A highly motivated B.Tech graduate with a passion for Webvvdfv
 						Development, User Experience, and Product Management. Possesses 5
 						years of freelancing experience, demonstrating strong team spirit,
 						critical thinking, and problem-solving skills. Eager to leverage
@@ -27,7 +27,7 @@ const Hero = () => {
 					</p>
 					<div className='mt-[2rem] flex-col space-y-6 sm:space-y-0 sm:flex sm:flex-row items-center sm:space-x-6'>
 						<button className='px-[2rem] bg-yellow-400 hover:bg-[#55e6a5] transition-all duration-200 py-[1rem] text-[18px] font-bold uppercase text-black flex items-center space-x-2'>
-							<ArrowDownTrayIcon className="w-[1.6rem]" />
+							<ArrowDownTrayIcon className='w-[1.6rem]' />
 							<p>Download Resume</p>
 						</button>
 					</div>
