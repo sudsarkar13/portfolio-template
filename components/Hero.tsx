c9b8dd9cdb2114eefcf332a71348d10061ca12e3
SUDEEPTA SARKAR <67630579+sudsarkar13@users.noreply.github.com>
2024-02-12 11:18:43 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
 import Particle from "./Particle";
+import TextEffect from "./TextEffect";
+import Image from "next/image";
 // import Error404 from "./errors/Error404";
 // import Error503 from "./errors/Error503";
 
@@ -9,9 +11,21 @@ const Hero = () => {
 		<div className="h-[90vh] bg-[url('/images/banner.jpg')] bg-cover bg-center ">
 			<Particle />
 			<div className='w-[80%] grid-cols-1 mx-auto grid lg:grid-cols-2 gap-[3rem] h-[100%] items-center'>
-				<h1 className="text-[35px] md:text-[50px] text-white font-extrabold">
-					HI, I'M <span className="text-yellow-400">SUDEEPTA!</span>
-				</h1>
+				<div>
+					<h1 className='text-[35px] md:text-[50px] text-white font-extrabold'>
+						HI, I'M <span className='text-yellow-400'>SUDEEPTA!</span>
+					</h1>
+					<TextEffect />
+				</div>
+				<div>
+					<Image
+						src='/images/Sudeepta.png'
+						alt='Sudeepta'
+						width={500}
+						height={200}
+						layout='responsive'
+					/>
+				</div>
 			</div>
 		</div>
 	);
